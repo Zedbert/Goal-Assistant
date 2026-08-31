d23d6b21940594392651b3b70b64bbefefb0dc05
Zedbert <florianweiner53@googlemail.com>
2026-08-31 18:49:41 +0200

    Add 6.3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C="goal-assistant-v6.2";
+const C="goal-assistant-v6.3";
 const OLD=["goal-assistant-v6","goal-assistant-v6.1"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./styles.css?v=6.2","./app.js?v=6.2","./manifest.json"])).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>OLD.includes(k)).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
